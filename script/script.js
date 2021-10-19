1c9cf4a560627b54b5d8e9e65a8c3c4a781c6891
min328 <kmj20212021@gmail.com>
2021-10-19 15:45:07 +0900

    product qna page on going

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 $(function(){
+
+    /* index.html 스크롤 애니메이션 */
     let slideNum = 1;
     let distance = 0;
 
@@ -21,5 +23,42 @@ $(function(){
             console.log("거리값 : " + distance);
             $("html").animate({"scrollTop" : distance}, 500);
         }
+    });
+
+    /* product.html 상품페이지에서 tab 버튼 클릭 시 페이지 탑 이동 */
+    // $(window).scroll(function(){
+    //     $("div#test").text($(window).scrollTop());
+    // });
+    $("main#productMain div.tab div.tab1").click(function(){
+        $("html").animate(
+            { "scrollTop" : "37"}
+            , 500
+        );
+    });
+
+    $("main#productMain div.tab div.tab2").click(function(){
+        $("html").animate(
+            { "scrollTop" : "2035"}
+            , 500
+        );
+    });
+
+    $("main#productMain div.tab div.tab3").click(function(){
+        $("html").animate(
+            { "scrollTop" : "2700"}
+            , 500
+        );
+    });
+
+    $("main#productMain div.tab div.tab4").click(function(){
+        $("html").animate(
+            { "scrollTop" : "2985"}
+            , 500
+        );
+    });
+
+    /* product.html qnaWriteBtn클릭시 문의사항 작성페이지로 이동 */
+    $("main#productMain button#qnaWriteBtn").click(function(){
+        window.location.href = "product_qna.html";
     });
 });
