02d3a1d231f6be9f9019fbcd804bf96023e7bd37
min328 <kmj20212021@gmail.com>
2021-10-24 23:41:01 +0900

    shop main page css changed

--- script/indexScript.js
+++ script/indexScript.js
@@ -184,27 +184,4 @@ $(function(){
         );
    });
 
-    /* 챗봇 버튼 클릭*/
-    $("div#chatbotBtn").click(function(){
-        $(this).children("div#chatAlarm").hide();
-        $(this).css({
-            "width" : "0"
-            ,"height" : "0"
-            ,"transition" : "0.5s"
-        });
-        $("div#chatMessage").delay(500).slideDown();
-    });
-    
-    /* 챗본 close버튼 클릭 */
-    $("div#chatMessage button#msgClose").click(function(){
-        $("div#chatMessage").slideUp();
-        $("div#chatbotBtn").css({
-            "width" : "60px"
-            ,"height" : "60px"
-            ,"transition" : "0.5s"
-        });
-        $("div#chatbotBtn").children("div#chatAlarm").show();
-    });
-
-    
 });
--- script/shopScript.js
+++ script/shopScript.js
@@ -1,18 +1,20 @@
 $(function(){
 
-    let productSet = [["상품명1", "가격1"],["상품명2", "가격2"],["상품명3", "가격3"],["상품명4", "가격4"],["상품명5", "가격5"],["상품명6", "가격6"],["상품명7", "가격7"],["상품명8", "가격8"]];
+    let productSet = [["상품명1", "가격1", "할인가격"],["상품명2", "가격2", "할인가격"],["상품명3", "가격3", "할인가격"],["상품명4", "가격4", "할인가격"],["상품명5", "가격5", "할인가격"],["상품명6", "가격6", "할인가격"],["상품명7", "가격7", "할인가격"],["상품명8", "가격8", "할인가격"]];
     
 
     for(i=0; i<productSet.length; i++) {
-        let imgSrc = "/images/productImage" + (i+1) +".jpg";
+        let imgSrc = "/images/product_" + (i+1) +".jpg";
         let title = productSet[i][0];
         let price = productSet[i][1];
+        let discounted = productSet[i][2];
         let productContainer = "<div class=\"itemBox\">"
         + "<div class=\"imgBox\">"
         + "<img src=\"" + imgSrc + "\">"
         + "</div>"
         + "<p class=\"title\">"+ title +"</p>"
         + "<p class=\"price\">"+ price +"</p>"
+        + "<p class=\"discounted\">"+ discounted +"</p>"
         + "</div>";
         console.log(productContainer);
         $("div#contentsList").append(productContainer);
