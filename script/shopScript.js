d723d877c00fe5e6449e6654c12254eea9cfb485
min328 <kmj20212021@gmail.com>
2021-10-25 15:03:13 +0900

    shop main page completed

--- script/shopScript.js
+++ script/shopScript.js
@@ -1,23 +1,43 @@
 $(function(){
 
-    let productSet = [["상품명1", "가격1", "할인가격"],["상품명2", "가격2", "할인가격"],["상품명3", "가격3", "할인가격"],["상품명4", "가격4", "할인가격"],["상품명5", "가격5", "할인가격"],["상품명6", "가격6", "할인가격"],["상품명7", "가격7", "할인가격"],["상품명8", "가격8", "할인가격"]];
+    let productSet = [["진영 '햇'단감 1.5kg", 11900],["청송 부사 사과 1kg", 9900],["제주 당도선별 GAP 감귤 2kg", 15900],["전남 고흥 프리미엄 석류 생과 2.5kg", 49800],["강원도 고랭지 알타리무 2kg", 15000],["전북 순창 유기농 블루베리 500g (냉동)", 16900],["양평 로메인 상추 1kg", 19800],["해남 대서감자 5kg", 12500]];
     
-
     for(i=0; i<productSet.length; i++) {
         let imgSrc = "/images/product_" + (i+1) +".jpg";
         let title = productSet[i][0];
         let price = productSet[i][1];
-        let discounted = productSet[i][2];
-        let productContainer = "<div class=\"itemBox\">"
+        let discounted = price * 0.7;
+        price = price.toLocaleString();
+        discounted = discounted.toLocaleString();
+
+        /* 상품 아이템 생성 */
+        let productContainer = "<div class=\"itemBox\" data-link=\"product_" + (i+1) + "\">"
         + "<div class=\"imgBox\">"
         + "<img src=\"" + imgSrc + "\">"
         + "</div>"
         + "<p class=\"title\">"+ title +"</p>"
         + "<p class=\"price\">"+ price +"</p>"
-        + "<p class=\"discounted\">"+ discounted +"</p>"
+        + "<p class=\"discounted\">"+ "<span class=\"disPercent\">30%</span>" +discounted +"</p>"
         + "</div>";
-        console.log(productContainer);
+
+        
+        // console.log(productContainer);
         $("div#contentsList").append(productContainer);
+
     }
+    
+    $("main#shopMain div.itemBox").click(function(){
+
+        let url = $(this).attr("data-link");
+
+        /* 포트폴리오용으로 인덱스 0번 아이템만 링크 연결 해두었음 */
+        if(url == "product_1") {
+            window.location.href = url + ".html";
+        } else {
+            alert("상품 상세 페이지는 1번 아이템을 클릭하여 확인해 주세요.");
+            return;
+        }
+    });
+
     
 });
